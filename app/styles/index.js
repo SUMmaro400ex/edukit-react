d86e74219774e4f4a2c70a696f2a4c2750fb1b4d
Andrea Perez <andrea.ocarina@gmail.com>
2017-04-18 23:47:52 -0400

    Update login form

--- app/styles/index.js
+++ app/styles/index.js
@@ -4,7 +4,7 @@ var styles = {
             marginBottom: 0
         },
         img:{
-            height: '3rem', 
+            height: '3rem',
             float: 'left'
         },
         login:{
@@ -13,10 +13,20 @@ var styles = {
         }
     },
     login:{
-        mainDiv:{
+        container:{
+            marginLeft: '40%',
+            marginRight: '40%',
+            marginTop: '15%',
+            border: '1px solid',
+            borderColor: '#C1C1C1',
+            borderRadius: 20
+        },
+        form:{
             textAlign: 'center',
-            height: '90vh', 
-            width: '100%'
+            margin: '10%'
+        },
+        input:{
+          color: '#D6C4F0'
         }
     }
 };
